dab13ce7462465545a3a03818ad2119e107bbf5c
BaymaxShao <91866296+BaymaxShao@users.noreply.github.com>
2025-02-19 05:58:35 +0000

    Deploying to gh-pages from @ BaymaxShao/PAW-Shao@a2ad17cc4556ae5b5edd3fd9b3fd7b8776f88ed4 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -63,7 +63,10 @@ ninja.data = [{
           window.location.href = "/al-folio/blog/2015/formatting-and-links/";
         
       },
-    },{id: "news-the-website-is-constructing",
+    },{id: "news-i-have-one-paper-about-real-time-ego-motion-tracking-for-various-endoscopy-accepted-by-icra-2025",
+          title: 'I have ONE paper about Real-time Ego-motion Tracking for Various Endoscopy, accepted by...',
+          description: "",
+          section: "News",},{id: "news-the-website-is-constructing",
           title: 'The Website is constructing.',
           description: "",
           section: "News",},{id: "projects-project-1",
