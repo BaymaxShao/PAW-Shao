c3f3af1463da9d1154857d0c71ef5c9a74a1cc14
BaymaxShao <91866296+BaymaxShao@users.noreply.github.com>
2025-03-08 14:10:25 +0000

    Deploying to gh-pages from @ BaymaxShao/PAW-Shao@0ba1491519f22ca1d82b33262ea7f42020a167be 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,8 +23,8 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/al-folio/publications/";
           },
-        },{id: "nav-cv",
-          title: "CV",
+        },{id: "nav-curriculum-vitae",
+          title: "Curriculum Vitae",
           description: "",
           section: "Navigation",
           handler: () => {
